e94634a67d4c6c4c8c94dbc2a8d2a2d5ee1a3759
huxiaoman7 <charlotte77_hu@sina.com>
2017-07-12 14:58:37 +0800

    modify_1

--- genPlate.py
+++ genPlate.py
@@ -1,4 +1,9 @@
 #coding=utf-8
+""" genPlate.py:生成随机车牌"""
+
+__author__ = "Huxiaoman"
+__copyright__ = "Copyright (c) 2017 "
+
 import PIL
 from PIL import ImageFont
 from PIL import Image
@@ -9,7 +14,6 @@ import os;
 from math import *
 import sys
 
-# font = ImageFont.truetype("Arial-Bold.ttf",14)
 
 index = {"京": 0, "沪": 1, "津": 2, "渝": 3, "冀": 4, "晋": 5, "蒙": 6, "辽": 7, "吉": 8, "黑": 9, "苏": 10, "浙": 11, "皖": 12,
          "闽": 13, "赣": 14, "鲁": 15, "豫": 16, "鄂": 17, "湘": 18, "粤": 19, "桂": 20, "琼": 21, "川": 22, "贵": 23, "云": 24,
@@ -137,7 +141,6 @@ def addNoise(img,sdev = 0.5,avg=10):
 
 class GenPlate:
 
-
     def __init__(self,fontCh,fontEng,NoPlates):
         self.fontC =  ImageFont.truetype(fontCh,43,0);
         self.fontE =  ImageFont.truetype(fontEng,60,0);
@@ -206,7 +209,7 @@ class GenPlate:
         plate.extend(b[1:7])
         return plateStr,plate
 
-
+    # 将生成的车牌图片写入文件夹，对应的label写入label.txt
     def genBatch(self, batchSize,pos,charRange, outputPath,size):
         if (not os.path.exists(outputPath)):
             os.mkdir(outputPath)
