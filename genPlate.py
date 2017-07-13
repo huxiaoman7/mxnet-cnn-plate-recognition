873f30b769e6cad694204770bc7b129b3787e207
huxiaoman7 <charlotte77_hu@sina.com>
2017-07-13 14:53:33 +0800

    add notes

--- genPlate.py
+++ genPlate.py
@@ -30,11 +30,10 @@ chars = ["京", "沪", "津", "渝", "冀", "晋", "蒙", "辽", "吉", "黑", "
 
 def AddSmudginess(img, Smu):
     rows = r(Smu.shape[0] - 50)
-
     cols = r(Smu.shape[1] - 50)
     adder = Smu[rows:rows + 50, cols:cols + 50];
     adder = cv2.resize(adder, (50, 50));
-    #   adder = cv2.bitwise_not(adder)
+    #adder = cv2.bitwise_not(adder)
     img = cv2.resize(img,(50,50))
     img = cv2.bitwise_not(img)
     img = cv2.bitwise_and(adder, img)
@@ -42,33 +41,28 @@ def AddSmudginess(img, Smu):
     return img
 
 def rot(img,angel,shape,max_angel):
-    """ 使图像轻微的畸变
-
+    """ 
+        添加放射畸变
         img 输入图像
         factor 畸变的参数
         size 为图片的目标尺寸
-
     """
     size_o = [shape[1],shape[0]]
-
     size = (shape[1]+ int(shape[0]*cos((float(max_angel )/180) * 3.14)),shape[0])
-
-
     interval = abs( int( sin((float(angel) /180) * 3.14)* shape[0]));
-
     pts1 = np.float32([[0,0],[0,size_o[1]],[size_o[0],0],[size_o[0],size_o[1]]])
     if(angel>0):
-
         pts2 = np.float32([[interval,0],[0,size[1]  ],[size[0],0  ],[size[0]-interval,size_o[1]]])
     else:
         pts2 = np.float32([[0,0],[interval,size[1]  ],[size[0]-interval,0  ],[size[0],size_o[1]]])
-
     M  = cv2.getPerspectiveTransform(pts1,pts2);
     dst = cv2.warpPerspective(img,M,size);
-
     return dst;
 
 def rotRandrom(img, factor, size):
+    """
+    添加透视畸变
+    """
     shape = size;
     pts1 = np.float32([[0, 0], [0, shape[0]], [shape[1], 0], [shape[1], shape[0]]])
     pts2 = np.float32([[r(factor), r(factor)], [ r(factor), shape[0] - r(factor)], [shape[1] - r(factor),  r(factor)],
@@ -77,11 +71,11 @@ def rotRandrom(img, factor, size):
     dst = cv2.warpPerspective(img, M, size);
     return dst;
 
-
-
 def tfactor(img):
+    """
+    添加饱和度光照的噪声
+    """
     hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV);
-
     hsv[:,:,0] = hsv[:,:,0]*(0.8+ np.random.random()*0.2);
     hsv[:,:,1] = hsv[:,:,1]*(0.3+ np.random.random()*0.7);
     hsv[:,:,2] = hsv[:,:,2]*(0.2+ np.random.random()*0.8);
@@ -90,11 +84,12 @@ def tfactor(img):
     return img
 
 def random_envirment(img,data_set):
+    """
+    添加自然环境的噪声    
+    """
     index=r(len(data_set))
     env = cv2.imread(data_set[index])
-
     env = cv2.resize(env,(img.shape[1],img.shape[0]))
-
     bak = (img==0);
     bak = bak.astype(np.uint8)*255;
     inv = cv2.bitwise_and(bak,env)
@@ -102,15 +97,20 @@ def random_envirment(img,data_set):
     return img
 
 def GenCh(f,val):
+    """
+    生成中文字符
+    """
     img=Image.new("RGB", (45,70),(255,255,255))
     draw = ImageDraw.Draw(img)
     draw.text((0, 3),val,(0,0,0),font=f)
     img =  img.resize((23,70))
     A = np.array(img)
-
     return A
 
 def GenCh1(f,val):
+    """
+    生成英文字符
+    """
     img=Image.new("RGB", (23,70),(255,255,255))
     draw = ImageDraw.Draw(img)
     draw.text((0, 2),val.decode('utf-8'),(0,0,0),font=f)
@@ -118,12 +118,18 @@ def GenCh1(f,val):
     return A
 
 def AddGauss(img, level):
+    """
+    添加高斯模糊
+    """ 
     return cv2.blur(img, (level * 2 + 1, level * 2 + 1));
 
 def r(val):
     return int(np.random.random() * val)
 
 def AddNoiseSingleChannel(single):
+    """
+    添加高斯噪声
+    """
     diff = 255-single.max();
     noise = np.random.normal(0,1+r(6),single.shape);
     noise = (noise - noise.min())/(noise.max()-noise.min())
@@ -156,7 +162,6 @@ class GenPlate:
 
     def draw(self,val):
         offset= 2 ;
-
         self.img[0:70,offset+8:offset+8+23]= GenCh(self.fontC,val[0]);
         self.img[0:70,offset+8+23+6:offset+8+23+6+23]= GenCh1(self.fontE,val[1]);
         for i in range(5):
@@ -171,8 +176,6 @@ class GenPlate:
             com = cv2.bitwise_or(fg,self.bg);
             com = rot(com,r(60)-30,com.shape,30);
             com = rotRandrom(com,10,(com.shape[1],com.shape[0]));
-            #com = AddSmudginess(com,self.smu)
-
             com = tfactor(com)
             com = random_envirment(com,self.noplates_path);
             com = AddGauss(com, 1+r(4));
